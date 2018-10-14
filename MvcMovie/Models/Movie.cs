e512c0b8aff3332e1e684b05cb6e30463a2e06db
Pedro Oliveira <pedrohsaoliveira@gmail.com>
2018-10-14 22:43:07 +0100

    "Add a model" step

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
